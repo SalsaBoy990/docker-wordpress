1e6730b39054527088047bb2d3b1ae7216ea7a9c
Gulácsi András <guland@protonmail.com>
2021-12-04 00:53:16 +0100

    new: Add owlcarousel styles and scripts to header and footer

--- src/wp-content/themes/gulacsi-bulma/functions.php
+++ src/wp-content/themes/gulacsi-bulma/functions.php
@@ -143,7 +143,12 @@ function gulacsi_bulma_scripts() {
 	wp_enqueue_style( 'gulacsi-bulma-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'gulacsi-bulma-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'gulacsi-bulma-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('gulacsi-bulma-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
+	
+	/* Owl Carousel v2.3.4 script */
+	wp_enqueue_script(
+	'gulacsi-owl-carousel', get_template_directory_uri() . '/inc/owl.carousel/dist/owl.carousel.js', array('jquery'), _S_VERSION, true);
+	wp_enqueue_script('gulacsi-custom-scripts', get_template_directory_uri() . '/js/custom.js', array('jquery'), _S_VERSION, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
