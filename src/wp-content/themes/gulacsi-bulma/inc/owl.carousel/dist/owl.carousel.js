4f5db0356f6e8f2c171cd9eab43e2d55bfcf62a4
Gulácsi András <guland@protonmail.com>
2021-12-04 00:51:44 +0100

    update: Change owl-carousel nav arrows (to material icons)

--- src/wp-content/themes/gulacsi-bulma/inc/owl.carousel/dist/owl.carousel.js
+++ src/wp-content/themes/gulacsi-bulma/inc/owl.carousel/dist/owl.carousel.js
@@ -2963,8 +2963,8 @@
 	Navigation.Defaults = {
 		nav: false,
 		navText: [
-			'<span aria-label="' + 'Previous' + '">&#x2039;</span>',
-			'<span aria-label="' + 'Next' + '">&#x203a;</span>'
+			'<span aria-label="' + 'Previous' + '"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z"/></svg></span>',
+			'<span aria-label="' + 'Next' + '"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><polygon points="6.23,20.23 8,22 18,12 8,2 6.23,3.77 14.46,12"/></g></svg></span>'
 		],
 		navSpeed: false,
 		navElement: 'button type="button" role="presentation"',
